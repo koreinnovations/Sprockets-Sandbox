90ba61b5f2220b9749a00c76227c2290da5a3320
Noah Lively <noah@koremedia.net>
2012-03-26 17:07:02 -0700

    Looks like we now have a system for linking nested controllers to specific js files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-
+  before_filter :specific_asset_path
   before_filter :load_js
+
+  def specific_asset_path
+    name = self.class.name
+    @controller_with_path = name.sub(/Controller$/, '').underscore
+  end
 
   def load_js
 
@@ -18,6 +23,7 @@ class ApplicationController < ActionController::Base
       'Members' => {
         members_index_path => 'Index',
         members_events_path => 'Events',
+        members_admin_index_path => 'Admin Index',
       },
     }
 
